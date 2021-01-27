88e4ba6c74e530de30995d604b84dad9d89d95f2
JaeSeong Park <a86007681@gmail.com>
2021-01-28 01:05:49 +0900

    docs : add Dtos
    add PlaceCategoryDto, PlaceDto

---
+++ src/main/java/com/koreait/baraON/dto/PlaceDto.java
@@ -0,0 +1,25 @@
+package com.koreait.baraON.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PlaceDto {
+	private int pNo;
+	private int sNo;
+	private int pcNo;
+	private String pTitle;
+	private String pDesc;
+	private String pContent;
+	private String pInfo;
+	private String pName;
+	private String pAddr;
+	private int Star;
+	private String pImg;
+	private int pConfirm;
+	private String pUrl;
+	private int pDelete;
+}
---
+++ src/main/java/com/koreait/baraON/dto/SellerDto.java
@@ -0,0 +1,15 @@
+package com.koreait.baraON.dto;
+
+import java.sql.Date;
+
+public class SellerDto {
+	private int sNo;
+	private String sId;
+	private String sPw;
+	private String sPhone;
+	private String sName;
+	private String sBirth;
+	private String sEmail;
+	private Date sRegDate;
+	
+}
