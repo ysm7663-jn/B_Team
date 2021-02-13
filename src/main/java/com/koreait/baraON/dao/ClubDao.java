981dd8742d7093c9a13b90f9ba941959d020610e
YunSeongMin <ysm7663@gmail.com>
2021-02-14 06:32:48 +0900

    2021-02-14

--- src/main/java/com/koreait/baraON/dao/ClubDao.java
+++ src/main/java/com/koreait/baraON/dao/ClubDao.java
@@ -19,8 +19,4 @@ public interface ClubDao {
 
 	public int clubUpdate(String c_title, int c_min, int c_max, String c_content, String c_startDate, String c_endDate, int c_no);
 	
-	public int updateStat(String m_enjoy, int m_no);
-
-
-
-}
+ }
